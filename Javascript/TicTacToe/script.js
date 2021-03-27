4445c84e11f5dd2e9bf92fb20213d7d9fe6072f9
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2021-03-27 09:53:44 +0530

    Add files via upload

--- Javascript/TicTacToe/script.js
+++ Javascript/TicTacToe/script.js
@@ -2,6 +2,8 @@ window.addEventListener("load", initEvents);
 
 var user_choice = "";
 var cpu_choice = "";
+var btns = [];
+var count = 0;
 function initEvents() {
     document.querySelector(".start-btn").addEventListener("click", startGame);
 }
@@ -27,6 +29,7 @@ function buildUI() {
         var li = document.createElement("li");
         var btn = document.createElement("button");
         btn.className = 'btn';
+        btns.push(btn);
         li.appendChild(btn);
         ul.appendChild(li);
 
@@ -36,4 +39,21 @@ function buildUI() {
 
 function userMove() {
     this.innerText = user_choice;
+    cpuPosition();
+}
+
+function cpuPosition() {
+    if(count <= 8) {
+        cpu_pos = parseInt(Math.random() * 9);
+        if(btns[cpu_pos].innerText == "X" || btns[cpu_pos].innerText == "0") {
+            console.log("Already occupied",cpu_pos);
+            cpuPosition();
+        }
+        else {
+            console.log("CPU Picked",cpu_pos);
+            btns[cpu_pos].innerText = cpu_choice;
+            btns[cpu_pos].disabled = 'on';
+            return
+        }
+    }
 }
